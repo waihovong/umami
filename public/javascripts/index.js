ffcf67a2555756eda644ee4fc69230cd75d9c530
tyndallye <tyndall.ye@gmail.com>
2019-05-25 02:05:25 +0930

    side nav

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -10,23 +10,31 @@ function currentSlide(n) {
 }
 
 function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("slides");
-    var dots = document.getElementsByClassName("dot");
-    if (n > slides.length) { 
-      slideIndex = 1; 
-    }
-    if (n < 1) { 
-      slideIndex = slides.length;
-    }
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-    }
+  var i;
+  var slides = document.getElementsByClassName("slides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
 
-    for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex - 1].style.display = "block";
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+}
+
+function openSM() {
+  document.getElementById("mySidemenu").style.width = "450px";
+}
+
+function closeSM() {
+  document.getElementById("mySidemenu").style.width = "0";
 }
 
 function toNextDiv() {
@@ -40,10 +48,10 @@ function scrollToTop() {
 }
 
 var address;
-function searchBarFunction(event){
-	address=document.getElementById("search-bar").value;
-    if(event.keyCode == 13){
+function searchBarFunction(event) {
+  address = document.getElementById("search-bar").value;
+  if (event.keyCode == 13) {
     window.location.href = "booking.html";
-    }
-	return false;
+  }
+  return false;
 }
