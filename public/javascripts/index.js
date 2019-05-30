75577e351e401c55e7836da32166046968fa765a
Wai Ho <waihovong@gmail.com>
2019-05-30 16:08:52 +0930

    changes

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -67,13 +67,3 @@ function searchBarFunction(event) {
   return false;
 }
 
-function loadText() {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      document.getElementsByClassName("ajax-text-change")[0].innerHTML = this.responseText;
-    }
-  }
-  xhttp.open("GET", "template.txt", true);
-  xhttp.send();
-}
